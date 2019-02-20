4dbf5bda5f64af871483b2b19de2033c10ee4a5f
Nemanja Jovic <nemanja.jovic@vtrade.local>
2019-02-20 15:24:04 +0100

    Adding get-activeuser function

--- EUC-ps4all/Public/Get-ActiveUser.ps1
+++ EUC-ps4all/Public/Get-ActiveUser.ps1
@@ -1,6 +1,29 @@
 Function Get-ActiveUser {
     [CmdletBinding()]
     param (
-        
+        # Parameter help description
+        [Parameter(Mandatory=$true,
+        Position=1)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $ComputerName
     )
+    process {
+        try {
+            $GetUsers = Invoke-Command -ComputerName $ComputerName -ScriptBlock {quser | Select-Object -skip 1} -ErrorAction Stop
+            $Userlist = New-Object System.Collections.ArrayList
+            foreach ($User in $GetUsers) {
+                $Username = ($User.substring(1)).split(" ")[0]
+                $Userlist += $Username
+            }
+            $Finallist = [PSCustomObject]@{
+                Hostname = $ComputerName
+                Activeuser = $Userlist -join ","
+            }
+            $Finallist
+        }
+        catch {
+            Write-Error "$_"
+        }
+    }
 }
