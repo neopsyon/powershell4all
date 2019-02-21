ac98845077a7b8a3f9945a903dc066d399a9f94f
Nemanja Jovic <nemanja.jovic@vtrade.local>
2019-02-21 10:45:44 +0100

    Added function Get-ComputerSite

--- ActiveDirectory-ps4all/Public/Get-ComputerSite.ps1
+++ ActiveDirectory-ps4all/Public/Get-ComputerSite.ps1
@@ -1,3 +1,26 @@
 Function Get-ComputerSite {
-    
+    [CmdletBinding()]
+    param (
+        # Name of the target computer.
+        [Parameter(Mandatory=$true,
+        Position=1)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $ComputerName
+    )
+    process {
+        try {
+            $SiteName = (Invoke-Command -ComputerName $ComputerName -ScriptBlock {
+                (Get-ItemProperty "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Group Policy\State\Machine" -Name "Site-Name").'site-name'
+            } -ErrorAction Stop)
+            $FoundSite = [PSCustomObject]@{
+                ComputerName = $ComputerName
+                SiteName = $SiteName
+            }
+            $FoundSite
+        }
+        catch {
+            Write-Error "$_" -ErrorAction Stop
+        }
+    }
 }
