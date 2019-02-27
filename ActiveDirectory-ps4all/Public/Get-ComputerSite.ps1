d4e9a979f01d4088d80dbf4bc12464b744038608
HurtzDonut01 <30447331+HurtzDonut01@users.noreply.github.com>
2019-02-27 15:50:26 -0500

    Update Get-ComputerSite.ps1
    Added Comment Based Help
    
    Added Function alias 'pcsite'
    
    Cleaned up Parameter declaration
    
    Updates to Process block
    - Support multiple computer names from parameter with ForEach loop
    - Add code to verify computer connection state
    - Reduce amount of code within Try..Catch block to support more precise error checking
    - Return results directly, rather than assigning to a variable first

--- ActiveDirectory-ps4all/Public/Get-ComputerSite.ps1
+++ ActiveDirectory-ps4all/Public/Get-ComputerSite.ps1
@@ -1,27 +1,70 @@
+<#
+    .SYNOPSIS
+        Returns the site code for the specified computer.
+    .DESCRIPTION
+        Queries the registry of a specified computer and returns value of the 'Site-Name' property found in 'HKLM:\..\Group Policy\..\Machine'.
+    .EXAMPLE
+        PS C:\> $Comps = 'PC1','PC2','PC3','PC4' | ForEach-Object {Get-ADComputer -Identity $PSItem}
+        PS C:\> $Comps | Get-ComputerSite
+        WARNING: [PC1] : Unable to connect to computer. Skipping.
+
+        ComputerName SiteName
+        ------------ --------
+        PC2          HQ
+        PC3          Branch1
+        PC4          Operations
+
+        Verfies the connectivity of each computer in $Comps, and if available, returns the site name for each.
+    .EXAMPLE
+        PS C:\> $Comps = 'PC1','PC2','PC3','PC4'
+        PS C:\> Get-ComputerSite -Name $Comps
+        WARNING: [PC1] : Unable to connect to computer. Skipping.
+
+        ComputerName SiteName
+        ------------ --------
+        PC2          HQ
+        PC3          Branch1
+        PC4          Operations
+
+        Verfies the connectivity of each computer in $Comps, and if available, returns the site name for each.        
+    .INPUTS
+        System.String
+    .OUTPUTS
+        PSCustomObject
+#>
 Function Get-ComputerSite {
     [CmdletBinding()]
-    param (
-        # Name of the target computer.
-        [Parameter(ValueFromPipeline=$true,
-        ValueFromPipelineByPropertyName=$true,
-        Position=0)]
+    [Alias('pcsite')]
+    Param (
+        [Parameter(ValueFromPipelineByPropertyName, Position=0)]
         [ValidateNotNullOrEmpty()]
-        [Alias('Computer','ComputerName')]
-        [string]$Name = $env:ComputerName
+        [Alias('ComputerName')]
+            [String[]]$Name = $env:ComputerName
     )
-    process {
-        try {
-            $SiteName = (Invoke-Command -ComputerName $Name -ScriptBlock {
-                (Get-ItemProperty 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Group Policy\State\Machine' -Name 'Site-Name').'site-name'
-            } -ErrorAction Stop)
-            $FoundSite = [PSCustomObject]@{
-                ComputerName = $Name
-                SiteName = $SiteName
-            }
-            $FoundSite
-        }
-        catch {
-            Write-Error "$_" -ErrorAction Stop
-        }
-    }
-}
+    
+    Process {
+        ForEach ($Machine in $Name) {
+            If (!(Test-Connection -ComputerName $Machine -Count 1 -Quiet)) {
+                Write-Warning ('[{0}] : Unable to connect to computer. Skipping.' -F $Machine)
+            } Else {
+                $SiteSplat = @{
+                    ComputerName= $Machine
+                    ScriptBlock = [ScriptBlock]::Create("Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Group Policy\State\Machine' -Name 'Site-Name'")
+                    ErrorAction = 'Stop'
+                }
+                
+                Try {
+                    $SiteCode = Invoke-Command @SiteSplat
+                } Catch {
+                    Write-Error -Exception $PSItem.Exception -Message $PSItem.Exception.Message
+                    Break
+                }
+
+                [PSCustomObject]@{
+                    ComputerName= $Machine
+                    SiteCode    = $SiteCode
+                }
+            } # Connectivity Check
+        } # ForEach Machine in Name
+    } # Process Block
+} # Function Get-ComputerSite
