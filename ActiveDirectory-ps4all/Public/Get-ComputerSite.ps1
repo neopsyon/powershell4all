385f154c82f17333f03aeafd3d429f25a4222fff
Nemanja Jovic <nemanja.jovic@vtrade.local>
2019-02-28 11:52:21 +0100

    Update Get-ComputerSite.ps1
    Reconstructing code layout.

--- ActiveDirectory-ps4all/Public/Get-ComputerSite.ps1
+++ ActiveDirectory-ps4all/Public/Get-ComputerSite.ps1
@@ -1,10 +1,13 @@
 <#
 .SYNOPSIS
 Returns the site code for the specified computer.
+
 .DESCRIPTION
 Queries the registry of a specified computer and returns value of the 'Site-Name' property found in 'HKLM:\..\Group Policy\..\Machine'.
+
 .PARAMETER Name
 Specifies the name of the distinguished computer that query is going to be executed against.
+
 .EXAMPLE
 PS C:\> Get-ADComputer -Filter "OperatingSystem -like '*Windows10*'" | Get-ComputerSite
 WARNING: [PC1] : Unable to connect to computer. Skipping.
@@ -16,6 +19,7 @@ PC3          Branch1
 PC4          Operations
 
 Verfies the connectivity of each computer in $Comps, and if available, returns the site name for each.
+
 .EXAMPLE
 PS C:\> $Comps = 'PC1','PC2','PC3','PC4'
 PS C:\> Get-ComputerSite -Name $Comps
@@ -26,9 +30,11 @@ ComputerName SiteName
 PC2          HQ
 PC3          Branch1
 PC4          Operations
-Verfies the connectivity of each computer in $Comps, and if available, returns the site name for each.        
+Verfies the connectivity of each computer in $Comps, and if available, returns the site name for each.
+
 .INPUTS
 System.String
+
 .OUTPUTS
 PSCustomObject
 #>
@@ -37,7 +43,8 @@ Function Get-ComputerSite {
     [OutputType([pscustomobject])]
     [Alias('pcsite')]
     Param (
-        [Parameter(ValueFromPipelineByPropertyName,
+        [Parameter(ValueFromPipeline=$true
+        ,ValueFromPipelineByPropertyName=$true,
         Position=0)]
         [ValidateNotNullOrEmpty()]
         [Alias('ComputerName','Computer')]
@@ -45,27 +52,23 @@ Function Get-ComputerSite {
     )
     Process {
         ForEach ($Machine in $Name) {
-            If ($false -eq (Test-Connection -ComputerName $Machine -Count 1 -Quiet)) {
-                Write-Warning ('[{0}] : Unable to connect to computer. Skipping.' -F $Machine)
-                Continue
+            Test-QuickConnect -Name $Machine
+            $SiteSplat = @{
+                ComputerName= $Machine
+                ScriptBlock = [ScriptBlock]::Create("Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Group Policy\State\Machine' -Name 'Site-Name'")
+                ErrorAction = 'Stop'
             }
-            Else {
-                $SiteSplat = @{
-                    ComputerName= $Machine
-                    ScriptBlock = [ScriptBlock]::Create("Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Group Policy\State\Machine' -Name 'Site-Name'")
-                    ErrorAction = 'Stop'
-                }
-                Try {
-                    $SiteCode = Invoke-Command @SiteSplat
-                } Catch {
-                    Write-Error -Exception $PSItem.Exception -Message $PSItem.Exception.Message
-                    Break
-                }
-                [PSCustomObject]@{
-                    ComputerName = $Machine
-                    SiteCode = $SiteCode
-                }
+            Try {
+                $SiteCode = Invoke-Command @SiteSplat
+            } 
+            Catch {
+                Write-Error -Exception $PSItem.Exception -Message $PSItem.Exception.Message
+                Break
             }
-        } 
+            [PSCustomObject]@{
+            ComputerName = $Machine
+            SiteCode = $SiteCode
+            }
+        }
     } 
-} 
+}  
