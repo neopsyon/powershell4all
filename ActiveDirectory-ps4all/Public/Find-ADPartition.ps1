49e5c6f454e987185b17f58b2096bca811e41588
Nemanja Jovic <nemanja.jovic@vtrade.local>
2019-03-01 15:13:35 +0100

    Adding logic to support export of all partition names. plus adding help
    Co-Authored-By: fatalgeek1 <fatalgeek1@users.noreply.github.com>

--- ActiveDirectory-ps4all/Public/Find-ADPartition.ps1
+++ ActiveDirectory-ps4all/Public/Find-ADPartition.ps1
@@ -5,8 +5,22 @@ Find AD partition based on your choice.
 .DESCRIPTION
 Function that queries Active Directory objects and finds out exact name of the partition, based on user input.
 
+.PARAMETER All
+Select this parameter in order to search for all Active Directory partition names.
+
 .PARAMETER PartitionName
 Name of the partition that you want to search for.
+
+.EXAMPLE
+PS C:\Windows\system32> Find-ADPartition -All
+
+Partition                                    
+---------                                    
+DC=ForestDnsZones,DC=local,DC=domain         
+DC=DomainDnsZones,DC=local,DC=domain         
+CN=Schema,CN=Configuration,DC=local,DC=domain
+CN=Configuration,DC=local,DC=domain          
+DC=local,DC=domain 
 
 .EXAMPLE
 PS C:\> Find-ADPartition -PartitionName Configuration
@@ -16,14 +30,16 @@ CN=Configuration,DC=domain,DC=local
 System.String
 
 .OUTPUTS
+PSCustomObject
 System.String
 #>
 Function Find-ADPartition {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName="All")]
     [OutputType([string])]
     param (
-        # Name of the partition that you want to find.
-        [Parameter(Mandatory=$true,
+        [Parameter(ParameterSetName="All")]
+        [switch]$All,
+        [Parameter(ParameterSetName="PartitionName",
         ValueFromPipeline=$true,
         Position=0)]
         [ValidateSet('Root','Configuration','Schema','ForestDNS','DomainDNS')]
@@ -31,20 +47,29 @@ Function Find-ADPartition {
     )
     Process {
         $Partitionlist = (Get-ADDomainController -Filter * | Select-Object -First 1).partitions
-        if ($PartitionName -eq 'Root') {
-            $Partitionlist | Where-Object {$_ -eq "DC=$($env:USERDNSDOMAIN.split('.')[0]),DC=$($env:USERDNSDOMAIN.split('.')[1])"}
+        if ($PSCmdlet.ParameterSetName -eq "All") {
+            foreach ($Item in $Partitionlist) {
+                [PSCustomObject]@{
+                    Partition = $Item;
+                }
+            }
         }
-        elseif ($PartitionName -eq 'Configuration') {
-            $Partitionlist | Where-Object {$_ -like "CN=Configuration,DC=$($env:USERDNSDOMAIN.split('.')[0]),DC=$($env:USERDNSDOMAIN.split('.')[1])"}
-        }
-        elseif ($PartitionName -eq 'Schema') {
-            $Partitionlist | Where-Object {$_ -like "CN=Schema,CN=Configuration,DC=$($env:USERDNSDOMAIN.split('.')[0]),DC=$($env:USERDNSDOMAIN.split('.')[1])"}
-        }
-        elseif ($PartitionName -eq 'ForestDNS') {
-            $Partitionlist | Where-Object {$_ -like "DC=ForestDNSZones,DC=$($env:USERDNSDOMAIN.split('.')[0]),DC=$($env:USERDNSDOMAIN.split('.')[1])"}
-        }
-        elseif ($PartitionName -eq 'DomainDNS') {
-            $Partitionlist | Where-Object {$_ -like "DC=DomainDNSZones,DC=$($env:USERDNSDOMAIN.split('.')[0]),DC=$($env:USERDNSDOMAIN.split('.')[1])"}
+        elseif ($PSCmdlet.ParameterSetName -eq "PartitionName") {
+            if ($PartitionName -eq 'Root') {
+                $Partitionlist | Where-Object {$_ -eq "DC=$($env:USERDNSDOMAIN.split('.')[0]),DC=$($env:USERDNSDOMAIN.split('.')[1])"}
+            }
+            elseif ($PartitionName -eq 'Configuration') {
+                $Partitionlist | Where-Object {$_ -like "CN=Configuration,DC=$($env:USERDNSDOMAIN.split('.')[0]),DC=$($env:USERDNSDOMAIN.split('.')[1])"}
+            }
+            elseif ($PartitionName -eq 'Schema') {
+                $Partitionlist | Where-Object {$_ -like "CN=Schema,CN=Configuration,DC=$($env:USERDNSDOMAIN.split('.')[0]),DC=$($env:USERDNSDOMAIN.split('.')[1])"}
+            }
+            elseif ($PartitionName -eq 'ForestDNS') {
+                $Partitionlist | Where-Object {$_ -like "DC=ForestDNSZones,DC=$($env:USERDNSDOMAIN.split('.')[0]),DC=$($env:USERDNSDOMAIN.split('.')[1])"}
+            }
+            elseif ($PartitionName -eq 'DomainDNS') {
+                $Partitionlist | Where-Object {$_ -like "DC=DomainDNSZones,DC=$($env:USERDNSDOMAIN.split('.')[0]),DC=$($env:USERDNSDOMAIN.split('.')[1])"}
+            }
         }
     }
 }
