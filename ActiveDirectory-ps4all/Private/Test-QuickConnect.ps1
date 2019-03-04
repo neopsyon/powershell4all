b2eaad4042a637fd017bd2f6373a8b07b6246d01
Nemanja Jovic <nemanja.jovic@vtrade.local>
2019-03-04 11:10:54 +0100

    Adding help for private helper function.

--- ActiveDirectory-ps4all/Private/Test-QuickConnect.ps1
+++ ActiveDirectory-ps4all/Private/Test-QuickConnect.ps1
@@ -1,3 +1,23 @@
+<#
+.SYNOPSIS
+Checking if remote computer is reachable.
+
+.DESCRIPTION
+Querying remote computer with the ping request, in case it is not responding, it will write an warning message and skip further actions against that computer.
+
+.PARAMETER Name
+Name of the computer that you want to query.
+
+.EXAMPLE
+Test-NetConnect -Name ComputerName
+WARNING: [test1] : Unable to connect to computer. Skipping.
+
+.INPUTS
+System.String
+
+.OUTPUTS
+System.String
+#>
 Function Test-QuickConnect {
     [CmdletBinding()]
     param (
